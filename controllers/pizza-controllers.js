26e7608e0626e8b5072b15bdcbb617abfd07c58a
ryan <ryan.williams93@aol.com>
2022-03-07 14:07:18 -0600

    created PUT and DELETE methods/ initialized all CRUD methods

--- controllers/pizza-controllers.js
+++ controllers/pizza-controllers.js
@@ -19,7 +19,7 @@ const pizzaController = {
         .then(dbPizzaData => {
             // if no pizza is found, send 404
             if (!dbPizzaData) {
-                res.status(404).json({ message: 'No pizza found with this id.'});
+                res.status(404).json({ message: 'No pizza found with this id, ya jerk!'});
                 return;
             }
             res.json(dbPizzaData);
@@ -37,6 +37,36 @@ const pizzaController = {
         .then(dbPizzaData => res.json(dbPizzaData))
         .catch(err => res.status(400).json(err));
     },
+
+    // update pizza by id
+    updatePizza({ params, body }, res) {
+        // With this .findOneAndUpdate() method, Mongoose finds a single document we want to update, then updates it and returns the updated document.
+        //  If we don't set that third parameter, { new: true }, it will return the original document.
+        // By setting the parameter to true, we're instructing Mongoose to return the new version of the document.
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        .then(dbPizzaData => {
+            if(!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id, ya freak!'});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    // delete pizza
+    deletePizza({ params }, res) {
+        // Like with updating, we could alternatively use .deleteOne() or .deleteMany(), but we're using the .findOneAndDelete() method because it provides a little more data in case the client wants it.
+        Pizza.findOneAndDelete({ _id: params.id })
+        .then(dbPizzaData => {
+            if(!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id, ya dork!'});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.status(400).json(err));
+    }
 };
 
 module.exports = pizzaController;
