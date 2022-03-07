195e5a6c7204e5332777e847e0c44039ec7cf92c
ryan <ryan.williams93@aol.com>
2022-03-07 16:41:40 -0600

    created controllers for comments

---
+++ controllers/comment-controller.js
@@ -0,0 +1,52 @@
+const { Comment, Pizza } = require('../models');
+
+const commentController = {
+    // add comment to pizza
+    addComment({ params, body }, res) {
+        console.log(body);
+        Comment.create(body)
+        .then(({ _id }) => {
+            return Pizza.findOneAndUpdate(
+                { _id: params.pizzaId },
+                // The $push method works just the same way that it works in JavaScript—it adds data to an array. 
+                { $push: { comments: _id } },
+                // Because we passed the option of new: true, we're receiving back the updated pizza (the pizza with the new comment included).
+                { new: true }
+            );
+        })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id!' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.json(err));
+    },
+    
+    // remove comment
+    removeComment({ params }, res) {
+        Comment.findOneAndDelete({ _id: params.commentId })
+        .then(deletedComment => {
+            if (!deletedComment) {
+                return res.status(404).json({ message: 'No comment with this id, ya nerd!' });
+            }
+            return Pizza.findOneAndUpdate(
+                { _id: params.pizzaId },
+                // We take the data and use it to identify and remove it from the associated pizza using the Mongo $pull operation.
+                { $pull: { comments: params.commentId } },
+                { new: true }
+            );
+        })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id, ya gerbil!' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.json(err));
+    }
+};
+
+module.exports = commentController;
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,25 +1,47 @@
 const { Schema, model } = require('mongoose');
 
 // We don't have to define the fields, as MongoDB will allow the data anyway, but for for clarity and usability, we should regulate what the data will look like.
-const PizzaSchema = new Schema({
-    // Using MongoDB and Mongoose, we simply instruct the schema that this data will adhere to the built-in JavaScript data types, including strings, Booleans, numbers, and so on.
-    pizzaName: {
-        type: String
+const PizzaSchema = new Schema(
+    {
+        // Using MongoDB and Mongoose, we simply instruct the schema that this data will adhere to the built-in JavaScript data types, including strings, Booleans, numbers, and so on.
+        pizzaName: {
+            type: String
+        },
+        createdBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date, 
+            // If no value is provided in this field when the user creates new data, the Date.now function will be executed and will provide a timestamp. This way we don't have to create the timestamp elsewhere and send that data.
+            default: Date.now
+        },
+        size: {
+            type: String,
+            default: 'Large'
+        },
+        // This indicates an array as the data type. You could also specify 'Array' in place of the brackets
+        toppings: [],
+        // We need to tell Mongoose to expect an ObjectId and to tell it that its data comes from the Comment model. 
+        comment: [
+            {
+                type: Schema.Types.ObjectId,
+                // The ref property is especially important because it tells the Pizza model which documents to search to find the right comments.
+                ref: 'Comment'
+            }
+        ]
     },
-    createdBy: {
-        type: String
-    },
-    createdAt: {
-        type: Date, 
-        // If no value is provided in this field when the user creates new data, the Date.now function will be executed and will provide a timestamp. This way we don't have to create the timestamp elsewhere and send that data.
-        default: Date.now
-    },
-    size: {
-        type: String,
-        default: 'Large'
-    },
-    // This indicates an array as the data type. You could also specify 'Array' in place of the brackets
-    toppings: []
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        // We set id to false because this is a virtual that Mongoose returns, and we don’t need it.
+        id: false
+    }
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
 });
 
 // Pizza model using PizzaSchema
